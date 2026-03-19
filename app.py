efa7abca2a4f17a5a59a44b7ba07bddc5da9a24c
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-18 21:26:33 -0300

    melhoria api

--- app.py
+++ app.py
@@ -1,19 +1,36 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import pickle
 
-app = FastAPI()
+app = FastAPI(
+    title="API de Predição de Diabetes",
+    description="API de Machine Learning para prever a ocorrência de diabetes com base em dados clínicos.",
+    version="1.0.0"
+)
 
-# carregar modelo
+class PatientData(BaseModel):
+    Pregnancies: int
+    Glucose: float
+    BloodPressure: float
+    SkinThickness: float
+    Insulin: float
+    BMI: float
+    DiabetesPedigreeFunction: float
+    Age: int
+
 with open("model_diabetes.pkl", "rb") as f:
     model = pickle.load(f)
 
-@app.get("/")
+@app.get("/", tags=["Status"])
 def home():
     return {"message": "API funcionando"}
 
-@app.post("/predict")
-def predict(data: dict):
-    values = list(data.values())
+@app.post("/predict", tags=["Predição"])
+def predict(data: PatientData):
+    values = list(data.dict().values())
     prediction = model.predict([values])
 
-    return {"prediction": int(prediction[0])}
+    return {
+        "prediction": int(prediction[0]),
+        "descricao": "Paciente com diabetes" if prediction[0] == 1 else "Paciente sem diabetes"
+    }
